c4e6f5433025f4fa2f0cf2b51a55d7dfbc858d3f
sandhyadav <66108338+sandhyadav@users.noreply.github.com>
2022-10-09 15:34:27 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+
+
+function getData() {  firebase.database().ref("/").on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key;
+       Room_names = childKey;
+      
+    });
+  });
+
+}
+
+getData();
+
